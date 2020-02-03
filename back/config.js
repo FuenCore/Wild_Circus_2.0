fcfe4a1eedcbde605b956d2eebb9e1d1f19e67c1
FuenCore <fuentes33160@gmail.com>
2020-02-03 13:38:10 +0100

    Last commit git add .!

--- back/config.js
+++ back/config.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
+// const connection = mysql.createConnection({
+//     host: process.env.HOST_CIRCUS,
+//     user: process.env.USER_CIRCUS,
+//     password: process.env.PW_CIRCUS,
+//     database: process.env.DB_CIRCUS
+// })
+
 const connection = mysql.createConnection({
-    host: process.env.HOST_CIRCUS,
-    user: process.env.USER_CIRCUS,
-    password: process.env.PW_CIRCUS,
-    database: process.env.DB_CIRCUS
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "wild_circus"
 })
 
 module.exports = connection;
